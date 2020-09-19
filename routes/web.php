086ac5556d74fe58cf329d064afa140cef090001
Pedro Vasconcelos <me@pedrovasconcelos.com>
2020-09-19 16:50:22 +0100

    Tome lá o token

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/test', function () {
+    return view('layouts.front');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
