63153fcc2b9218442d325eba6d37389e345ad3fc
Pedro Vasconcelos <me@pedrovasconcelos.com>
2020-10-04 19:37:00 +0100

    Form error messages

--- resources/views/components/form-field.blade.php
+++ resources/views/components/form-field.blade.php
@@ -1,6 +1,9 @@
 <div class="mt-6">
-    <label for="email" class="block text-sm font-medium leading-5 text-gray-700">{{$label}}</label>
+    <label @isset($id) for="{{ $id }}" @endisset class="block text-sm font-medium leading-5 text-gray-700">{{$label}}</label>
     <div class="mt-1 rounded-md shadow-sm">
         {{ $slot }}
     </div>
+    @isset($id)
+    @error( $id ) <span class="error">{{ $message }}</span> @enderror
+    @endisset
 </div>
