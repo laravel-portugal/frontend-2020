c9f1e868c44fd1f6671b0c16f123c443d6df36e5
Tiago Fernandes <tiagoff@gmail.com>
2020-10-31 16:18:57 +0000

    Added missing test to fake cliente

--- tests/Unit/ApiClientTest.php
+++ tests/Unit/ApiClientTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Http\Clients\FakeClient;
 use App\Http\Clients\RealClient;
 use App\Http\Clients\ApiClient;
 use Tests\TestCase;
@@ -12,5 +13,12 @@ class ApiClientTest extends TestCase
     public function it_resolves_an_api_client_instance()
     {
         $this->assertInstanceOf(RealClient::class, resolve(ApiClient::class));
+    }
+
+    /** @test **/
+    public function it_resolves_an_api_client_to_fake_instance()
+    {
+        ApiClient::fake();
+        $this->assertInstanceOf(FakeClient::class, resolve(ApiClient::class));
     }
 }
