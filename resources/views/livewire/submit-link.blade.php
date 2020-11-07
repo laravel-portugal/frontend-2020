340ed2c5515492f06d4200333c8c2334e7d91297
Joao Patricio <ijpatricio@gmail.com>
2020-11-07 15:46:25 +0000

    Fixes tags columns, were too many. Minor tweaks in form body.

--- resources/views/livewire/submit-link.blade.php
+++ resources/views/livewire/submit-link.blade.php
@@ -96,49 +96,69 @@
                               class="form-textarea block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"
                               wire:model="description"></textarea>
                 </x-form-field>
-                <div
-                    class="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                <div class="mt-6 sm:mt-5 sm:grid sm:grid-cols-2 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                     <fieldset>
                         <legend class="text-base font-medium text-gray-900">Tags</legend>
                         <div class="mt-4">
-                            @foreach( $availableTags as $tag )
+                            @foreach($availableTags as $tag)
                                 <div class="relative flex items-start">
                                     <div class="flex items-center h-5">
-                                        <input id="tags[{{ $loop->index }}" type="checkbox"
-                                               class="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
-                                               wire:model="tags.{{$tag['id']}}">
+                                        <input
+                                            wire:model="tags.{{ $tag['id'] }}"
+                                            id="tags[{{ $loop->index }}]"
+                                            type="checkbox"
+                                            class="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+                                        >
                                     </div>
                                     <div class="ml-3 text-sm leading-5">
-                                        <label for="tags[{{ $loop->index }}"
-                                               class="font-medium text-gray-700">{{$tag['name']}}</label>
+                                        <label
+                                            for="tags[{{ $loop->index }}]"
+                                            class="font-medium text-gray-700"
+                                        >
+                                            {{ $tag['name'] }}
+                                        </label>
                                     </div>
                                 </div>
                             @endforeach
                         </div>
                     </fieldset>
                 </div>
-                <div class="border-t border-gray-200 py-5 w-11/12 md:w-2/3 mx-auto">
+                <div class="border-t border-gray-200 mt-5 py-5 w-11/12 md:w-2/3 mx-auto">
                     <div class="flex justify-start">
                         <span class="inline-flex rounded-md shadow-sm">
-                          <button type="button"
-                                  class="py-2 px-4 border border-gray-300 rounded-md text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">Cancel</button>
+                            <button
+                                type="button"
+                                class="py-2 px-4 border border-gray-300 rounded-md text-sm leading-5
+                                       font-medium text-gray-700 hover:text-gray-500 focus:outline-none
+                                       focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50
+                                       active:text-gray-800 transition duration-150 ease-in-out"
+                            >
+                                Cancel
+                            </button>
                         </span>
                         <span class="ml-3 inline-flex rounded-md shadow-sm">
-                            <button type="submit" disabled="disabled"
-                                    class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out disabled:bg-indigo-300 disabled:cursor-not-allowed">
+                            <button
+                                type="submit"
+                                disabled="disabled"
+                                class="inline-flex justify-center py-2 px-4 border border-transparent
+                                        text-sm leading-5 font-medium rounded-md text-white bg-indigo-600
+                                        hover:bg-indigo-500 focus:outline-none focus:border-indigo-700
+                                        focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150
+                                         ease-in-out disabled:bg-indigo-300 disabled:cursor-not-allowed"
+                            >
                                 Save
                             </button>
-                            <div wire:loading>
-                                Em submissão
-                            </div>
-                            <div>
-                                {{ json_encode($response) }}
-                                @isset($response['status']) {{ $response['status'] }} @endisset
-                                @isset($response['message']) {{ $response['message'] }} @endisset
-                            </diV>
                         </span>
                     </div>
                 </div>
+                <div wire:loading>
+                    Em submissão
+                </div>
+                <pre>
+                    {{ json_encode($response) }}
+                    @isset($response['status']) {{ $response['status'] }} @endisset
+                    @isset($response['message']) {{ $response['message'] }} @endisset
+                </pre>
             </div>
         </div>
     </form>
