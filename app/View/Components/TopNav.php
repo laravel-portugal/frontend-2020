dd66258e31d28a3703e835d0d6842db5a9a41e09
Joao Patricio <ijpatricio@gmail.com>
2020-12-19 21:31:48 +0000

    staging tag:4

--- app/View/Components/TopNav.php
+++ app/View/Components/TopNav.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class top-nav extends Component
+class TopNav extends Component
 {
     /**
      * Create a new component instance.
